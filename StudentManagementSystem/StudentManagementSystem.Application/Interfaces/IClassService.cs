7a5c8c357ae05961cda47b69217fb1c07bc98cef
Abhi Patelm <abhipatel181998@gmail.com>
2026-01-15 14:56:14 +0530

    Add Application layer service interfaces.

---
+++ StudentManagementSystem/StudentManagementSystem.Application/Interfaces/IClassService.cs
@@ -0,0 +1,11 @@
+﻿using StudentManagementSystem.Application.DTOs;
+
+namespace StudentManagementSystem.Application.Interfaces
+{
+    public interface IClassService
+    {
+        Task<ClassDto> CreateClassAsync(CreateClassDto dto);
+        Task<List<ClassDto>> GetAllClassesAsync();
+        Task BulkImportClassesAsync(List<CreateClassDto> classes);
+    }
+}
---
+++ StudentManagementSystem/StudentManagementSystem.Application/Interfaces/IStudentService.cs
@@ -0,0 +1,13 @@
+﻿using StudentManagementSystem.Application.DTOs;
+
+namespace StudentManagementSystem.Application.Interfaces
+{
+    public interface IStudentService
+    {
+        Task<StudentDto> CreateStudentAsync(CreateStudentDto dto);
+        Task<StudentDto> UpdateStudentAsync(int id, UpdateStudentDto dto);
+        Task<bool> DeleteStudentAsync(int id);
+        Task<StudentDto?> GetStudentByIdAsync(int id);
+        Task<List<StudentDto>> GetAllStudentsAsync(int pageNumber, int pageSize, string? searchTerm = null);
+    }
+}
