5f59302973e45b85002e5bd8044c22130b8d8e12
Abhi Patelm <abhipatel181998@gmail.com>
2026-01-15 14:04:15 +0530

    Add EF Core DbContext and SQL Server configuration.

--- StudentManagementSystem/StudentManagementSystem.API/Program.cs
+++ StudentManagementSystem/StudentManagementSystem.API/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using StudentManagementSystem.Infrastructure.Data;
+
 namespace StudentManagementSystem.API
 {
     public class Program
@@ -13,6 +16,10 @@ namespace StudentManagementSystem.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            // Configure EF Core DbContext.
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
