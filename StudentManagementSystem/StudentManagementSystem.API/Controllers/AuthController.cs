53512e663994dddc5e6f5f56faffe3498359a2a0
Abhi Patelm <abhipatel181998@gmail.com>
2026-01-15 15:31:16 +0530

    Add API controller for auth APIs.

---
+++ StudentManagementSystem/StudentManagementSystem.API/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace StudentManagementSystem.API.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController(IConfiguration config) : ControllerBase
+    {
+        private readonly IConfiguration _config = config;
+
+        [HttpPost("login")]
+        public IActionResult Login(string username, string password)
+        {
+            // Hardcoded credentials (allowed)
+            if (username != "admin" || password != "admin123")
+                return Unauthorized("Invalid credentials");
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, username)
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_config["Jwt:Key"]!)
+            );
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(
+                    int.Parse(_config["Jwt:ExpiryMinutes"]!)
+                ),
+                signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+            );
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token)
+            });
+        }
+    }
+}
