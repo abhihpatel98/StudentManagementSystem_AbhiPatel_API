66551260d1748f06f211400dbff48fb0594b72d7
Abhi Patelm <abhipatel181998@gmail.com>
2026-01-15 15:07:59 +0530

    Add Class repository interface and class implementation.

---
+++ StudentManagementSystem/StudentManagementSystem.Application/Interfaces/IClassRepository.cs
@@ -0,0 +1,12 @@
+﻿using StudentManagementSystem.Domain.Entities;
+
+namespace StudentManagementSystem.Application.Interfaces
+{
+    public interface IClassRepository
+    {
+        Task<Class?> GetByIdAsync(int id);
+        Task<List<Class>> GetAllAsync();
+        Task AddRangeAsync(List<Class> classes);
+        Task SaveChangesAsync();
+    }
+}
---
+++ StudentManagementSystem/StudentManagementSystem.Infrastructure/Repositories/ClassRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagementSystem.Application.Interfaces;
+using StudentManagementSystem.Domain.Entities;
+using StudentManagementSystem.Infrastructure.Data;
+
+namespace StudentManagementSystem.Infrastructure.Repositories
+{
+    public class ClassRepository(ApplicationDbContext context) : IClassRepository
+    {
+        private readonly ApplicationDbContext _context = context;
+
+        public async Task AddRangeAsync(List<Class> classes) => await _context.Classes.AddRangeAsync(classes);
+
+        public async Task<List<Class>> GetAllAsync() => await _context.Classes.ToListAsync();
+
+        public async Task<Class?> GetByIdAsync(int id) => await _context.Classes.FindAsync(id);
+
+        public async Task SaveChangesAsync() => await _context.SaveChangesAsync();
+    }
+}
